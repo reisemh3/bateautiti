12cc81a2f10ed12b731a6f4021aab0e93e654e6f
Alexandre <reisemh3@gmail.com>
2023-03-13 12:14:23 +0100

    partie amdin ajouté

--- src/components/PublicNav.vue
+++ src/components/PublicNav.vue
@@ -30,8 +30,8 @@
         </a>
       </div>
       <div>
-        <a href="#"
-          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+        <a href="/admin"
+          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">Administration</a>
       </div>
     </div>
   </nav>
--- src/views/admin/Layout.vue
+++ src/views/admin/Layout.vue
@@ -1,15 +1,30 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-    <div class="admin">
-      <PublicNav />
-      Admin Layout
-      <router-view />
-      <PublicFoot />
+  <div class="admin">
+    <!-- <PublicNav /> -->
+    <div class="ad_contain">
+      <div id="ad_side">
+        <AdminNav />
+      </div>
+      <div id="ad_display" class="relative md:ml-64 bg-blueGray-100 w-full">
+        <router-view />
+      </div>
     </div>
-  </template>
+    <PublicFoot />
+  </div>
+</template> 
 
 <script>
+import AdminNav from '@/components/AdminNav.vue'
+import PublicNav from '@/components/PublicNav.vue'
+import PublicFoot from '@/components/PublicFoot.vue'
+
 export default {
   name: 'AdminLayout',
+  components: {
+    // PublicNav,
+    AdminNav,
+    PublicFoot,
+  },
 }
 </script>
